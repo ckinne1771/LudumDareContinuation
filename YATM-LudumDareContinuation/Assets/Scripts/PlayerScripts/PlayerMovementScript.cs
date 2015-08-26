7172e0ab136d5ce2b47584d49f49cfb917ef1286
ckinne1771 <ckinney1771@gmail.com>
2015-08-26 09:48:11 +0100

    Jumping and Double Jump Added
    TODO: Camera, Attacking, Pickups, Level design, Enemies

--- YATM-LudumDareContinuation/Assets/Scripts/PlayerScripts/PlayerMovementScript.cs
+++ YATM-LudumDareContinuation/Assets/Scripts/PlayerScripts/PlayerMovementScript.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class PlayerMovementScript : MonoBehaviour {
 
 	public float speed = 10.0f;
+	public float jumpPower = 20.0f;
+
+	private bool jump = false;
+	private bool doubleJump = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -20,5 +24,42 @@ public class PlayerMovementScript : MonoBehaviour {
 			float newX = transform.position.x - (1.0f * speed * Time.deltaTime);
 			transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
 		}
+
+		if(Input.GetKeyDown(KeyCode.W))
+		   {
+			//Debug.Log ("Phase 1");
+			Debug.Log (jump.ToString ());
+			if (jump == false)
+			{
+				//Debug.Log ("Phase 2");
+				Jumping ();
+				//gameObject.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up*jumpPower*Time.deltaTime);
+				jump = true;
+			}
+			else if( jump == true && doubleJump == false)
+			{
+				//Debug.Log ("Phase 3");
+				Jumping ();
+				//gameObject.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up*jumpPower*Time.deltaTime);
+				doubleJump = true;
+			}
+
+
 	}
+}
+
+	void OnCollisionEnter2D( Collision2D other)
+	{
+		if (other.gameObject.tag == "Floor") {
+			jump = false;
+			doubleJump = false;
+		}
+	}
+
+	void Jumping()
+	{
+		Debug.Log("Jumping");
+		gameObject.GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * jumpPower * speed);
+
+}
 }
