1ab3ef8f45f3660ae2dcfbe7cfe73e425065a4d8
ckinne1771 <ckinney1771@gmail.com>
2015-08-26 09:18:55 +0100

    Project created. Added basic left right movement.
    TODO; Add jumping, attacking, rest of the game.

---
+++ YATM-LudumDareContinuation/Assets/Scripts/PlayerScripts/PlayerMovementScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+	public float speed = 10.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.D)) {
+			float newX = transform.position.x + (1.0f * speed * Time.deltaTime);
+			transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			float newX = transform.position.x - (1.0f * speed * Time.deltaTime);
+			transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+		}
+	}
+}
