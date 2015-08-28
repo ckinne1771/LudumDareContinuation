49525c86f9d958ad7abad088dd7d04f91b19e64f
Matthew Mitchell <matt.mancer@gmail.com>
2015-08-28 14:00:46 +0100

    Add wall jump

--- YATM-LudumDareContinuation/Assets/Scripts/PlayerScripts/Controller.cs
+++ YATM-LudumDareContinuation/Assets/Scripts/PlayerScripts/Controller.cs
@@ -11,32 +11,45 @@ public class Controller : MonoBehaviour {
     [SerializeField] private float jumpAcceleration;
     [SerializeField] private float jumpTimer;
     [SerializeField] private float gravity;
+    
+    [SerializeField] private float wallJumpAcceleration;
+    [SerializeField] private float maxWallJumpVelocity;
 
     private Vector2 velocity = new Vector2(0.0f, 0.0f);
     private bool grounded = false;
     private bool jumping = false;
     private bool doubleJumped = false;
+    private float againstWall = 0.0f;
+    private float wallJumping = 0.0f;
     private float timer = 0.0f;
 
     private void Update() {
         float axis = Input.GetAxisRaw("Horizontal");
 
-        if (axis != 0.0f) {
-            float sign = Mathf.Sign(velocity.x);
+        if (wallJumping == 0.0f) {
+            if (axis != 0.0f) {
+                float sign = Mathf.Sign(velocity.x);
 
-            if (Mathf.Abs(velocity.x) < maxWalkingVelocity) {
-                velocity.x += axis * walkingAcceleration * Time.deltaTime;
-                if (Mathf.Abs(velocity.x) > maxWalkingVelocity) {
-                    velocity.x = maxWalkingVelocity * sign;
+                if (Mathf.Abs(velocity.x) < maxWalkingVelocity) {
+                    velocity.x += axis * walkingAcceleration * Time.deltaTime;
+                    if (Mathf.Abs(velocity.x) > maxWalkingVelocity) {
+                        velocity.x = maxWalkingVelocity * sign;
+                    }
+                } else {
+                    velocity.x += axis * runningAcceleration * Time.deltaTime;
+                    if (Mathf.Abs(velocity.x) > maxVelocity) {
+                        velocity.x = maxVelocity * sign;
+                    }
                 }
-            } else {
-                velocity.x += axis * runningAcceleration * Time.deltaTime;
-                if (Mathf.Abs(velocity.x) > maxVelocity) {
-                    velocity.x = maxVelocity * sign;
-                }
+            } else if (grounded) {
+                velocity.x = 0.0f;
             }
         } else {
-            velocity.x = 0.0f;
+            velocity.x += wallJumping * wallJumpAcceleration * Time.deltaTime;
+            if (Mathf.Abs(velocity.x) > maxWallJumpVelocity) {
+                velocity.x = maxWallJumpVelocity;
+                wallJumping = 0.0f;
+            }
         }
 
         if (grounded) {
@@ -47,7 +60,12 @@ public class Controller : MonoBehaviour {
         } else {
             velocity.y -= gravity * Time.deltaTime;
 
-            if (!doubleJumped && Input.GetButtonDown("Jump")) {
+            if (againstWall != 0.0f && Input.GetButtonDown("Jump")) {
+                velocity.x = 0.0f;
+                velocity.y = jumpVelocity;
+                wallJumping = againstWall;
+                againstWall = 0.0f;
+            } else if (!doubleJumped && Input.GetButtonDown("Jump")) {
                 doubleJumped = true;
                 jumping = true;
                 velocity.y = jumpVelocity;
@@ -72,9 +90,13 @@ public class Controller : MonoBehaviour {
     
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground")) {
-            grounded = true;
-            doubleJumped = true;
-            velocity.y = 0.0f;
+            if (collision.contacts[0].normal == Vector2.up) {
+                grounded = true;
+                doubleJumped = true;
+                velocity.y = 0.0f;
+            } else if (collision.contacts[0].normal != -Vector2.up) {
+                againstWall = collision.contacts[0].normal.x;
+            }
         }
     }
 
@@ -82,6 +104,7 @@ public class Controller : MonoBehaviour {
         if (grounded) {
             grounded = false;
             doubleJumped = false;
+            againstWall = 0.0f;
         }
     }
 }
