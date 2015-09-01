09063ae1b10df352df008838b18e2b7ac400a791
ckinne1771 <ckinney1771@gmail.com>
2015-09-01 16:05:00 +0100

    Started adding enemies. Slightly broke movement
    BUG: "Invisible wall" effect created sine I implements enemy raycast
    detection. I suspect this is interfering somewhat with the movement or
    collision system. I will need to consult with you Matthew as to how to
    fix this.
    
    TODO: Finish enemy implementation. Fix bugs. Add more to level.

---
+++ YATM-LudumDareContinuation/Assets/Scripts/EnemyScripts/EnemyStatScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStatScript : MonoBehaviour {
+
+
+	public int health;
+	public int speed;
+	public int attack;
+
+	private Vector3 playerPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+		//playerPosition = new Vector3 (GameObject.Find ("Player").transform.position.x, GameObject.Find ("Player").transform.position.y, GameObject.Find ("Player").transform.position.z);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerPosition = new Vector3 (GameObject.Find ("Player").transform.position.x, GameObject.Find ("Player").transform.position.y, GameObject.Find ("Player").transform.position.z);
+		//Debug.Log (playerPosition.ToString ());
+
+		RaycastHit2D hitLeft = Physics2D.Raycast (transform.position, Vector2.left);
+		RaycastHit2D hitRight = Physics2D.Raycast (transform.position, Vector2.right);
+		if (hitLeft.collider != null) {
+			//Debug.Log ("hit");
+			float distance = Mathf.Abs (hitLeft.point.x - transform.position.x);
+			Debug.Log (distance.ToString ());
+			if (distance <= 3.0f  && hitLeft.collider.tag=="Player") {
+
+				//transform.LookAt (new Vector3 (playerPosition.x, transform.position.y, transform.position.z));
+				transform.Translate (Vector3.left * speed * Time.deltaTime);
+			}
+		}
+		if (hitRight.collider != null) {
+			float distance = Mathf.Abs (hitLeft.point.x - transform.position.x);
+			if (distance <= 3.0f && hitRight.collider.tag=="Player") {
+				
+				//transform.LookAt (new Vector3 (playerPosition.x, transform.position.y, transform.position.z));
+				transform.Translate (Vector3.right * speed * Time.deltaTime);
+			}
+		}
+
+	}
+}
